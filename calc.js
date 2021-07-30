b134ef20ad4cbbff4fc95cd954c5eba470bc36bb
wahyupambudi <wahyupambudi823@gmail.com>
2021-07-31 00:57:11 +0700

    Update calc.js

--- calc.js
+++ calc.js
@@ -10,10 +10,10 @@ tombol.addEventListener("click", function (e) {
 		screen.value = "";
 	} else if (nilaiTombol == "Del") {
 		screen.value = screen.value.slice(0, -1);
+	} else if (nilaiTombol == "=" && screen.value == "") {
+		alert("Silahkan Masukkan Angka!");
 	} else if (nilaiTombol == "=") {
 		screen.value = eval(screen.value);
-	} else if (screen.value === "1" ) {
-		alert(":asdasd")
 	} else {
 		screen.value += nilaiTombol;
 	}
